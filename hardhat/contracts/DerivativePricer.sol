05045cb43bda804779f3a057e37c6101d5f579e6
Zain <zainfox2002@gmail.com>
2022-09-11 16:39:21 +0100

    added bond pricing function

--- hardhat/contracts/DerivativePricer.sol
+++ hardhat/contracts/DerivativePricer.sol
@@ -86,4 +86,65 @@ contract DerivativesPricer {
         return tot;
     }
 
+
+    /** @dev       Calculates the forward price of a bond
+      * @param s   Price of the bond
+      * @param t   Time to maturity of the contract
+      * @param r   Interest rate
+      * @param d   Dividend payment per quarter
+      * @param t_n Time until the next dividend payment
+      * @param f_r Forward rate
+      * @return p  Calculated forward price of the bond
+      */
+    function getBondPrice(
+        uint256 b,
+        uint256 t,
+        uint256 r,
+        uint256 c,
+        uint256 t_n,
+        uint256 f_r
+        )
+        external returns (uint256) {
+            p = s * (1 + r * t) - getQuarterlyDividend(t, t_n, d, f_r);
+            return p;
+        }
+
+    /** @dev        Calculates the total coupon payment for the above function
+      * @param t    Time to maturity of the contract
+      * @param t_n  Time until the next coupon payment
+      * @param d    Coupon payment per quarter
+      * @param f_r  Forward rate
+      * @return tot The total coupon payment
+      */
+    function getQuarterlyCouponPayment(
+        uint256 t,
+        uint256 t_n,
+        uint256 c,
+        uint256 f_r
+    ) external returns (uint256) {
+        uint256[] times = []
+        uint256 counter = t;
+        if t_n <= t {
+            counter -= t_n;
+            times.push(counter);
+        } else {
+            return 0;
+        }
+
+        while true {
+            counter -= 3;
+            if counter > 0 {
+                times.push(counter);
+            } else {
+                break
+            }
+        }
+        uint256 tot = 0;
+        for (uint i=0; i < counter.length; i++) {
+            tot += c * (1 + times[i]/12 * f_r);
+        }
+
+        return tot;
+    } 
+
 }
